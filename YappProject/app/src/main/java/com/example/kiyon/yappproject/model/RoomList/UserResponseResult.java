73d2200aad06b4c2d5efb55542f2721d251607d2
yoo <youhee09@naver.com>
2018-11-13 00:33:05 +0900

    층돌수정

--- YappProject/app/src/main/java/com/example/kiyon/yappproject/model/RoomList/UserResponseResult.java
+++ YappProject/app/src/main/java/com/example/kiyon/yappproject/model/RoomList/UserResponseResult.java
@@ -6,4 +6,7 @@ public class UserResponseResult implements Serializable {
 
     public String user_name;
     public String user_pic;
+    public String kakao_id; // user 고유 id값
+    public int iscreater; // 해당 변수가 1일 경우 방장유저 0일 경우 일반 유저
+    public boolean isUserChecked = false;
 }
